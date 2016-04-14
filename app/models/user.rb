4c0c4db75d4000904aaeaeb61aab2cda4dd21f64
Selvakumar <e1graje@MC02GCB8CDRJC.local>
2016-04-13 22:18:40 -0700

    tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,28 +20,28 @@ class User < ActiveRecord::Base
       'count' => 10,
       'html' => '$10 Discount <BR> On Any Premium <BR>Baked Edibles',
       'class' => 'two',
-      'image' =>  ActionController::Base.helpers.asset_path(
+      'image' =>  ActionController::Base.helpers.asset_url(
         'refer/1/discount.jpg')
     },
     {
       'count' => 30,
       'html' => '3 FREE Cana Cupcakes<br> & $10 Discount<br>On Cana Birthday Cake',
       'class' => 'three',
-      'image' => ActionController::Base.helpers.asset_path(
+      'image' => ActionController::Base.helpers.asset_url(
         'refer/2/3-cupcakes.jpg')
     },
     {
       'count' => 100,
       'html' => '5 FREE Cana Cupcakes <br> & $20 Discount On <br> Cana Birthday Cake',
       'class' => 'four',
-      'image' => ActionController::Base.helpers.asset_path(
+      'image' => ActionController::Base.helpers.asset_url(
         'refer/3/Gourmet-Cupcakes.jpg')
     },
     {
       'count' => 500,
       'html' => 'FREE 15 pc. Cannabis <br>Birthday Cake Valued <br>At $150.00',
       'class' => 'five',
-      'image' => ActionController::Base.helpers.asset_path(
+      'image' => ActionController::Base.helpers.asset_url(
         'refer/4/finalimg.jpg')
     }
   ]
