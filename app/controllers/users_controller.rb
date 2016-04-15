cfd91d91f19ef467792d6c7a3920c8b6d7eb6176
Selvakumar <e1graje@MC02GCB8CDRJC.local>
2016-04-14 22:56:00 -0700

    tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
     current_ip = IpAddress.find_by_address(address)
     if current_ip.nil?
       current_ip = IpAddress.create(address: address, count: 1)
-    elsif current_ip.count > 2
+    elsif current_ip.count > 10
       logger.info('IP address has already appeared three times in our records.
                  Redirecting user back to landing page.')
       return redirect_to root_path
