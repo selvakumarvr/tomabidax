8ae001bb56ec73dc41f0bf897a85d2999ae05d8f
Selvakumar <e1graje@MC02GCB8CDRJC.local>
2016-12-02 23:35:32 -0700

    tested

---
+++ db/migrate/20130127063937_devise_create_admin_users.rb
@@ -0,0 +1,8 @@
+class DeviseCreateAdminUsers < ActiveRecord::Migration
+  def migrate(direction)
+    super
+    # Create a default user
+    AdminUser.create!(:email => 'tech@tomabidax.com', :password => 'password123', :password_confirmation => 'h@rry5t3ch') if direction == :up
+  end
+
+end
